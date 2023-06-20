66bd0f4c6f768dbefbe7318f333142d2b5b6b948
Trizahwambua <wambuatrizzah5@gmail.com>
2023-06-20 10:19:15 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main - print _putchar
+ * is writte by the character c
  *return 0
  */
 
@@ -14,6 +15,6 @@ int main(void)
 		_putchar('h')
 		_putchar('a')
 		_putchar('r')
-		_putchar('n')
+		_putchar('\n')
 		return (0)
 }
