3cadf64089af413324c44da30450b4e14af63be3
Trizahwambua <wambuatrizzah5@gmail.com>
2023-06-22 15:36:25 -0700

    6

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef 'main.h'
+#define 'main.h'
+
+void positive_or_negative(int i);
+int  largest_numbers(int a, int b, int c)
+void print_remaining_days(int day, int month, int year)
+int convert_day(int month, int day)
+
+#endif	
